335d523db69b87a336b9a311a8da5b0d795cf203
eliad <eliad.haim@gmail.com>
2025-04-25 15:06:39 +0300

    Add formik to toy edit

--- src/pages/ToyEdit.jsx
+++ src/pages/ToyEdit.jsx
@@ -1,3 +1,8 @@
+import { Formik, Form, Field } from 'formik'
+import * as Yup from 'yup'
+import { useState, useEffect, useRef, Fragment } from "react"
+import { useParams, useNavigate } from "react-router-dom"
+
 // import { toyService } from '../services/toy.service.js'
 import { toyService } from '../services/toy.service.remote.js'
 
@@ -9,8 +14,6 @@ import { showErrorMsg, showSuccessMsg } from "../services/event-bus.service.js"
 
 import { ToyLabelsPicker } from "../cmps/ToyLabelsPicker.jsx"
 import { Link } from "react-router-dom"
-import { useState, useEffect, useRef } from "react"
-import { useParams, useNavigate } from "react-router-dom"
 import { Loader } from "../cmps/Loader.jsx"
 
 export function ToyEdit() {
@@ -22,8 +25,11 @@ export function ToyEdit() {
     const [toyToEdit, setToyToEdit] = useState({ ...toyService.getEmptyToy() })
     const [toyEditLabels, setToyEditLabels] = useState(null)
 
+    const imageUrlRegex = useRef(/\.(jpeg|jpg|gif|png|webp|bmp|svg)$/i)
+
     const hasChanges = useRef(false)
-    useConfirmTabClose(hasChanges)
+    useConfirmTabClose(hasChanges.current)
+
 
     useEffect(() => {
         if (toyId) {
@@ -43,28 +49,25 @@ export function ToyEdit() {
             })
     }
 
-    function handleChange({ target }) {
-        var { name, value } = target
-        if (name === 'price') value = +value
+    // function handleChange({ target }) {
+    //     var { name, value } = target
+    //     if (name === 'price') value = +value
 
-        setToyToEdit(prev => ({ ...prev, [name]: value }))
-        hasChanges.current = true
-    }
+    //     setToyToEdit(prev => ({ ...prev, [name]: value }))
+    //     // hasChanges.current = true
+    // }
 
     function onSaveLabels(labelsPicked) {
         setToyToEdit(prev => ({ ...prev, labels: labelsPicked }))
         hasChanges.current = true
     }
 
-    function onSave(ev) {
-        ev.preventDefault()
+    function onSave(toyToSave) {
+        if (!toyToSave?._id) toyToSave.createdAt = Date.now()
 
-        if (!toyToEdit?._id) toyToEdit.createdAt = Date.now()
+        toyToSave.imgUrl = imageUrlRegex.current.test(toyToSave.imgUrl) ? toyToSave.imgUrl : 'src/assets/img/no img.jpg'
 
-        const imageUrlRegex = /\.(jpeg|jpg|gif|png|webp|bmp|svg)$/i;
-        toyToEdit.imgUrl = imageUrlRegex.test(toyToEdit.imgUrl) ? toyToEdit.imgUrl : 'src/assets/img/no img.jpg'
-
-        toyActions.saveToy(toyToEdit)
+        toyActions.saveToy(toyToSave)
             .then(() => {
                 showSuccessMsg('toy saved')
                 navigate('/toy')
@@ -76,16 +79,19 @@ export function ToyEdit() {
 
     }
 
+    const SignupSchema = Yup.object().shape({
+        name: Yup.string().required('Name is required'),
+        price: Yup.number().required('Price is required').min(0, 'Price must be positive'),
+        imgUrl: Yup.string().matches(imageUrlRegex.current, 'Must be a valid image URL')
+    })
+
     if (toyId && !toyToEdit._id) return <Loader />
-
-    const { name, price, imgUrl, labels } = toyToEdit
-
     return (
         <section className="toy-edit">
 
             <h2>{toyToEdit._id ? "Edit Toy" : "Add Toy"}</h2>
 
-            <form onSubmit={onSave}>
+            {/* <form onSubmit={onSave}>
                 <label htmlFor="name">Name:</label>
                 <input type="text" name='name' id="name" value={name} onChange={handleChange} required />
                 <label htmlFor="name">Price:</label>
@@ -96,7 +102,57 @@ export function ToyEdit() {
                 <ToyLabelsPicker labels={labels} onSaveLabels={onSaveLabels} toyEditLabels={toyEditLabels} />
                 <button className="save-btn">Save</button>
                 <Link to='/toy'><button>Back to list</button></Link>
-            </form>
+            </form> */}
+
+
+            <Formik
+                initialValues={toyToEdit}
+                validationSchema={SignupSchema}
+                onSubmit={(values) => onSave({ ...values, labels: toyEditLabels })}
+            >
+                {({ errors, touched }) => (
+                    <Form>
+                        <div className='lable'>Name:</div>
+
+                        <Field type="text" name="name" id='name' placeholder='Enter toy name' />
+                        {errors.name && touched.name && <div className='error-msg'>{errors.name}</div>}
+
+                        <div className='lable'>Price: </div>
+                        <Field type="number" name="price" id='price' placeholder='Enter toy price' />
+                        {errors.price && touched.price && <div className='error-msg'>{errors.price}</div>}
+
+                        {/* <Field name='price' >
+                            {({ field, form }) => (
+                                <Fragment>
+                                    <input
+                                        type="number"
+                                        name='price'
+                                        id='price'
+                                        {...field}
+                                        value={field.value || ''}
+                                        placeholder="Enter toy price"
+                                        onChange={e => form.setFieldValue("price", e.target.value)}
+                                    />
+                                    {form.errors.price && form.touched.price && (
+                                        <div className="error-msg">{form.errors.price}</div>
+                                    )}
+                                </Fragment>
+                            )}
+
+                        </Field> */}
+
+                        <div className='lable'>Image URL: </div>
+                        <Field name="imgUrl" id='imgUrl' placeholder='Enter toy image url' />
+                        {errors.imgUrl && touched.imgUrl && <div className='error-msg'>{errors.imgUrl}</div>}
+
+                        <div className='form-btns flex justify-between'>
+                            <button type='submit' className="save-btn">Save</button>
+                            <Link to='/toy'><button>Back to list</button></Link>
+                        </div>
+                    </Form>
+                )}
+            </Formik>
         </section>
     )
 }
+
