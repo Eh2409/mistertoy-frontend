7f51063773dee05909328c6bbc2398fba6fb9565
eliad <eliad.haim@gmail.com>
2025-04-30 23:12:05 +0300

    Compile scss to css

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 
 // import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './assets/style/main.css'
+import './assets/style/css/main.css'
 import { RootCpm } from './RootCpm'
 
 createRoot(document.getElementById('root')).render(
