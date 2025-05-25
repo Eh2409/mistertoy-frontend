f901833eb0eabc77bfeb3b83e6ed1e5703ca9b36
eliad <eliad.haim@gmail.com>
2025-05-25 11:33:13 +0300

    Add filter accordion

--- src/cmps/ToyFilter.jsx
+++ src/cmps/ToyFilter.jsx
@@ -21,6 +21,11 @@ export function ToyFilter({ filterBy, onSetFilterBy, toysLabels }) {
     const [filterByToEdit, setFilterByToEdit] = useState({ name, price, labels, inStock, manufacturer, type, brand })
     const debounce = useRef(utilService.debounce(onSetFilterBy, 1000))
 
+    const [isFilterExpand, setIsFilterExpand] = useState(false)
+
+    function onToggleMobileFilter() {
+        setIsFilterExpand(!isFilterExpand)
+    }
 
     useEffectOnUpdate(() => {
         debounce.current(filterByToEdit)
@@ -55,78 +60,87 @@ export function ToyFilter({ filterBy, onSetFilterBy, toysLabels }) {
 
     return (
         <section className="toy-filter">
-            <h2>Toy Filter</h2>
-            <form >
-                <Box sx={{ minWidth: 120 }}>
+            <h2>
+                <span> Toy Filter </span>
+                <button
+                    className='filter-expand-btn'
+                    onClick={onToggleMobileFilter}>
+                    {isFilterExpand ? "▲" : "▼"}
+                </button>
+            </h2>
+            <div className={`filter-accordion ${isFilterExpand ? 'active' : ''}`}>
+                <form >
+                    <Box sx={{ minWidth: 120 }}>
 
-                    <TextField
-                        id="name"
-                        label="By Name"
-                        variant="outlined"
-                        type="text"
-                        name="name"
-                        className='text-field'
-                        value={filterByToEdit.name}
-                        onChange={handleChange}
-                    />
-                </Box>
-
-                <Box>
-                    <TextField
-                        id="price"
-                        label="By Min Price"
-                        variant="outlined"
-                        type="number"
-                        name="price"
-                        className='text-field'
-                        value={filterByToEdit.price || ''}
-                        onChange={handleChange}
-                    />
-                </Box>
-
-                <Box>
-                    <FormControl fullWidth>
-                        <InputLabel id="demo-simple-select-label">By Stock</InputLabel>
-                        <Select
-                            labelId="demo-simple-select-label"
-                            id="inStock"
-                            name="inStock"
-                            value={filterByToEdit.inStock === undefined ? 'all' : String(filterByToEdit.inStock)}
-                            label="inStock"
+                        <TextField
+                            id="name"
+                            label="By Name"
+                            variant="outlined"
+                            type="text"
+                            name="name"
+                            className='text-field'
+                            value={filterByToEdit.name}
                             onChange={handleChange}
-                        >
-                            <MenuItem value={"all"}>All</MenuItem>
-                            <MenuItem value={"true"}>In Stock</MenuItem>
-                            <MenuItem value={"false"}>Out of Stock</MenuItem>
-                        </Select>
-                    </FormControl>
-                </Box>
+                        />
+                    </Box>
+
+                    <Box>
+                        <TextField
+                            id="price"
+                            label="By Min Price"
+                            variant="outlined"
+                            type="number"
+                            name="price"
+                            className='text-field'
+                            value={filterByToEdit.price || ''}
+                            onChange={handleChange}
+                        />
+                    </Box>
+
+                    <Box>
+                        <FormControl fullWidth>
+                            <InputLabel id="demo-simple-select-label">By Stock</InputLabel>
+                            <Select
+                                labelId="demo-simple-select-label"
+                                id="inStock"
+                                name="inStock"
+                                value={filterByToEdit.inStock === undefined ? 'all' : String(filterByToEdit.inStock)}
+                                label="inStock"
+                                onChange={handleChange}
+                            >
+                                <MenuItem value={"all"}>All</MenuItem>
+                                <MenuItem value={"true"}>In Stock</MenuItem>
+                                <MenuItem value={"false"}>Out of Stock</MenuItem>
+                            </Select>
+                        </FormControl>
+                    </Box>
 
 
 
-                <ToyLabelsPickerUi
-                    name='Brands'
-                    options={toysLabels.brands}
-                    labels={filterByToEdit.brand}
-                    onSaveLabels={(labels) => onSaveLabels(labels, 'brand')}
-                />
+                    <ToyLabelsPickerUi
+                        name='Brands'
+                        options={toysLabels.brands}
+                        labels={filterByToEdit.brand}
+                        onSaveLabels={(labels) => onSaveLabels(labels, 'brand')}
+                    />
 
-                <ToyLabelsPickerUi
-                    name='Types'
-                    options={toysLabels.types}
-                    labels={filterByToEdit.type}
-                    onSaveLabels={(labels) => onSaveLabels(labels, 'type')}
-                />
+                    <ToyLabelsPickerUi
+                        name='Types'
+                        options={toysLabels.types}
+                        labels={filterByToEdit.type}
+                        onSaveLabels={(labels) => onSaveLabels(labels, 'type')}
+                    />
 
-                <ToyLabelsPickerUi
-                    name='Manufacturers'
-                    options={toysLabels.manufacturers}
-                    labels={filterByToEdit.manufacturer}
-                    onSaveLabels={(labels) => onSaveLabels(labels, 'manufacturer')}
-                />
+                    <ToyLabelsPickerUi
+                        name='Manufacturers'
+                        options={toysLabels.manufacturers}
+                        labels={filterByToEdit.manufacturer}
+                        onSaveLabels={(labels) => onSaveLabels(labels, 'manufacturer')}
+                    />
 
 
-            </form>
+                </form>
+            </div>
         </section >
     )
 }
